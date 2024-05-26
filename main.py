e396c3851b133e831817ebe3b6c3554009c4ff64
olegpasenko <odpash.itmo@gmail.com>
2024-05-26 10:02:20 +0300

    added dockerfile and business logic

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from flask import Flask, request
 from twilio.twiml.voice_response import VoiceResponse
 
+import message_processing
+
 app = Flask(__name__)
 
-prompt = {"role": "system",
-          "content": "Ты ассистент команды аватарекс. Твоя задача продавать бананы, представляйся Анастасией."}
-calls = []
+hi_message = "Добро пожаловать. Что бы вы хотели узнать?"
+prompt = [{"role": "system",
+           "content": "Ты ассистент команды аватарекс. Твоя задача продавать бананы, представляйся Анастасией."},
+          {'role': 'assistant', 'content': hi_message}]
+calls = {}
 
 
 @app.route("/voice", methods=['GET', 'POST'])
 def voice():
-    print(request.values)
-    resp = VoiceResponse()
-    resp.say("Привет от Олега для Влада")
-    resp.record()
+    global calls
 
+    recording_url = request.values.get('RecordingUrl', None)
+    call_id = request.values.get('CallSid', None)
+    print(f'{call_id=}, {recording_url=}')
+    resp = VoiceResponse()
+    if recording_url and call_id in calls.keys():
+        message_history = calls[call_id]
+        answer, message_history = message_processing.execute(recording_url, message_history)
+        calls[call_id] = message_history
+        resp.say(answer)
+    if not call_id:
+        calls[call_id] = prompt
+        resp.say(hi_message)
+    resp.record()
     return str(resp)
 
 
